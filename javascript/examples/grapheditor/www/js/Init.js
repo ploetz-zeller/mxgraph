cf5046bf815aa3a5d1a511ea549c4efa2ac947f8
Jochen Linnemann <jochen.linnemann@p-und-z.de>
2018-05-16 15:32:25 +0200

    Use P+Z specific init

--- javascript/examples/grapheditor/www/js/Init.js
+++ javascript/examples/grapheditor/www/js/Init.js
@@ -1,21 +1,45 @@
-// urlParams is null when used for embedding
-window.urlParams = window.urlParams || {};
-
-// Public global variables
-window.MAX_REQUEST_SIZE = window.MAX_REQUEST_SIZE  || 10485760;
-window.MAX_AREA = window.MAX_AREA || 15000 * 15000;
+﻿// Public global variables
+var MAX_REQUEST_SIZE = 10485760;
+var MAX_AREA = 10000 * 10000;
 
 // URLs for save and export
-window.EXPORT_URL = window.EXPORT_URL || '/export';
-window.SAVE_URL = window.SAVE_URL || '/save';
-window.OPEN_URL = window.OPEN_URL || '/open';
-window.RESOURCES_PATH = window.RESOURCES_PATH || 'resources';
-window.RESOURCE_BASE = window.RESOURCE_BASE || window.RESOURCES_PATH + '/grapheditor';
-window.STENCIL_PATH = window.STENCIL_PATH || 'stencils';
-window.IMAGE_PATH = window.IMAGE_PATH || 'images';
-window.STYLE_PATH = window.STYLE_PATH || 'styles';
-window.CSS_PATH = window.CSS_PATH || 'styles';
-window.OPEN_FORM = window.OPEN_FORM || 'open.html';
+var EXPORT_URL = PZ.Sy.Std.Navigation.toAbsoluteUrl('~/{storagecollection}/{tenant}/{data}/{lcid}/BasePlugin/Diagram/ExportDiagram');
+var SAVE_URL = PZ.Sy.Std.Navigation.toAbsoluteUrl('~/{storagecollection}/{tenant}/{data}/{lcid}/BasePlugin/Diagram/SaveDiagramEditor');
+var RESOURCES_PATH = PZ.Sy.Std.Navigation.toAbsoluteUrl('~/{storagecollection}/{tenant}/{data}/{lcid}/Resources/DiagramEditorResources');
+var RESOURCE_BASE = RESOURCES_PATH + '/';
+var STENCIL_PATH = PZ.Sy.Std.Navigation.toAbsoluteUrl('~/Images/DiagramEditor');
+var IMAGE_PATH = PZ.Sy.Std.Navigation.toAbsoluteUrl('~/Images/DiagramEditor');
+var STYLE_PATH = 'styles';
+var CSS_PATH = 'styles';
+var OPEN_FORM = 'open.html';
+
+// Specifies connection mode for touch devices (at least one should be true)
+var tapAndHoldStartsConnection = false;
+var showConnectorImg = true;
+
+// Parses URL parameters. Supported parameters are:
+// - lang=xy: Specifies the language of the user interface.
+// - touch=1: Enables a touch-style user interface.
+// - storage=local: Enables HTML5 local storage.
+// - chrome=0: Chromeless mode.
+var urlParams = (function (url) {
+    var result = new Object();
+    var idx = url.lastIndexOf('?');
+
+    if (idx > 0) {
+        var params = url.substring(idx + 1).split('&');
+
+        for (var i = 0; i < params.length; i++) {
+            idx = params[i].indexOf('=');
+
+            if (idx > 0) {
+                result[params[i].substring(0, idx)] = params[i].substring(idx + 1);
+            }
+        }
+    }
+
+    return result;
+})(window.location.href);
 
 // Sets the base path, the UI language via URL param and configures the
 // supported languages to avoid 404s. The loading of all core language
@@ -24,6 +48,10 @@ window.OPEN_FORM = window.OPEN_FORM || 'open.html';
 // files (the special bundle and the default bundle) is disabled to
 // save a GET request. This requires that all resources be present in
 // each properties file since only one file is loaded.
-window.mxBasePath = window.mxBasePath || '../../../src';
-window.mxLanguage = window.mxLanguage || urlParams['lang'];
-window.mxLanguages = window.mxLanguages || ['de'];
+mxLoadResources = false;
+mxLoadStylesheets = false;
+mxLanguage = PZ.Sy.Std.Cultures.getByLcid(PZ.Sy.Std.Navigation.getRouteValue('lcid', 1033)).ISO2;
+mxDefaultLanguage = "none";
+mxImageBasePath = IMAGE_PATH;
+var STYLE_FONTFAMILY = "Segoe UI";
+var STYLE_FONTSIZE = "12px";
